ac7ccfd45fd4881c4f48c7f23e937e986469ec77
PopeYang <101554426+PopeYang@users.noreply.github.com>
2024-08-20 16:02:04 +0800

    update label loop

--- LabelLoop.f90
+++ LabelLoop.f90
@@ -7,17 +7,25 @@ contains
 
         ! 使用带标签的DO循环遍历数字
         do 200 i = 1, 10
+            if (mod(i, 10) == 0) then
+                goto 300
+            endif
+    
             if (mod(i, 5) == 0) then
                 goto 210
             endif
-
+    
             write(*,*) 'Value of i: ', i
             goto 200
-            
-210         continue
+    
+    210     continue
             write(*,*) 'i is a multiple of 5: ', i
-            
-200     continue  ! 这是编号为200的行，循环结束后会到达这里
+            goto 200
+    
+    300     continue
+            write(*,*) 'i is a multiple of 10: ', i
+    
+    200     continue  ! 这是编号为200的行，循环结束后会到达这里
         
     end subroutine processLoop
     
